6acf49b3893e03be2666412d3c6498fe9d9973f4
idontreallywolf <nedim.kanat.lp@gmail.com>
2024-10-15 02:30:06 +0200

    Remove redundant import statements

--- app/src/main/java/com/example/tryggakampus/presentation/MainScreen.kt
+++ app/src/main/java/com/example/tryggakampus/presentation/MainScreen.kt
@@ -2,14 +2,14 @@ package com.example.tryggakampus.presentation
 
 import AppBar
 import android.annotation.SuppressLint
-import android.util.Log
+
 import androidx.activity.compose.BackHandler
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectHorizontalDragGestures
-import androidx.compose.foundation.layout.Arrangement
+
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -24,7 +24,7 @@ import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
+
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
