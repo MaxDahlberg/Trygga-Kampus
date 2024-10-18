06da97822c66d0d73c0aadeb8e61c7709ec5f515
Nedkan <nedim.kanat.lp@gmail.com>
2024-10-18 10:33:48 +0200

    Cache stories (#18)
    * install datastore dependency
    
    * extend context with datastore singleton
    
    * refactor repository to a singleton object
    
    * refactor viewmodel to load from datasource if cache time not exceeded
    
    * clean up

--- app/src/main/java/com/example/tryggakampus/presentation/storiesPage/StoriesPageViewModel.kt
+++ app/src/main/java/com/example/tryggakampus/presentation/storiesPage/StoriesPageViewModel.kt
@@ -1,21 +1,52 @@
 package com.example.tryggakampus.presentation.storiesPage
 
-import android.util.Log
-import androidx.compose.runtime.MutableState
+import android.content.Context
 import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.remember
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.longPreferencesKey
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.tryggakampus.dataStore
 import com.example.tryggakampus.domain.model.StoryModel
-import com.example.tryggakampus.domain.repository.StoryRepository
 import com.example.tryggakampus.domain.repository.StoryRepositoryImpl
+import com.google.firebase.firestore.Source
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
 
 class StoriesPageViewModel: ViewModel() {
     var stories = mutableStateListOf<StoryModel>()
         private set
 
-    suspend fun loadStories() {
-        stories.clear()
-        stories.addAll(StoryRepositoryImpl().getAllStories())
-        Log.d("LOAD", "stories: ${stories.size}")
+    fun loadStories(context: Context) {
+        viewModelScope.launch {
+            stories.clear()
+            val lastFetchTimeKey = longPreferencesKey("stories_last_fetch_time")
+            val lastFetchTime: Long = context.dataStore.data
+                .map { preferences -> preferences[lastFetchTimeKey] ?: 0L }
+                .first()
+
+            val currentTimeMillis = System.currentTimeMillis()
+            val timeDifference = (currentTimeMillis - lastFetchTime) / 1000
+            val source = if (timeDifference >= 20) {
+                Source.SERVER
+            } else {
+                Source.CACHE
+            }
+
+            stories.addAll(StoryRepositoryImpl.getAllStories(source))
+
+            if (source == Source.SERVER) {
+                updateStoriesFetchTime(context)
+            }
+        }
+    }
+
+    private suspend fun updateStoriesFetchTime(context: Context) {
+        val lastFetchTimeKey = longPreferencesKey("stories_last_fetch_time")
+
+        context.dataStore.edit { settings ->
+            settings[lastFetchTimeKey] = System.currentTimeMillis()
+        }
     }
 }
