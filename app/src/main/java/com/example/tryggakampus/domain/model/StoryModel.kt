627ea1dc0effd6aefcba3a0845b797be919990b6
Max Dahlberg <max.dahlberg0275@stud.hkr.se>
2025-10-17 01:00:28 +0200

    Updated the story model to include comment ids in a list and timestamp of creation time.

--- app/src/main/java/com/example/tryggakampus/domain/model/StoryModel.kt
+++ app/src/main/java/com/example/tryggakampus/domain/model/StoryModel.kt
@@ -1,14 +1,14 @@
 package com.example.tryggakampus.domain.model
 
-import com.google.firebase.Timestamp
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class StoryModel(
-    var id: String? = null,
+    var id: String = "",
     val title: String? = null,
     val userId: String? = null,
     val author: String? = null,
     val content: String = "No content",
-    // val createdAt: Timestamp = Timestamp.now()
+    val commentIds: List<String> = emptyList(),
+    val createdAt: Long = System.currentTimeMillis()
 )
