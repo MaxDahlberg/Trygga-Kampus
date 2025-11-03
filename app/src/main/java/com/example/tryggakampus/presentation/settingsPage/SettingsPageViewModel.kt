1401de0b3b7ef5c72bdd5a6ade5f425863b39c1e
Max Dahlberg <max.dahlberg0275@stud.hkr.se>
2025-11-03 13:19:31 +0100

    Removed the bottom bar in the settings page.

--- app/src/main/java/com/example/tryggakampus/presentation/settingsPage/SettingsPageViewModel.kt
+++ app/src/main/java/com/example/tryggakampus/presentation/settingsPage/SettingsPageViewModel.kt
@@ -1,32 +1,15 @@
-// app/src/main/java/com/example/tryggakampus/presentation/settingsPage/SettingsPageViewModel.kt
 package com.example.tryggakampus.presentation.settingsPage
 
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
-import com.example.tryggakampus.util.LanguageManager
 import java.util.*
 
 class SettingsPageViewModel : ViewModel() {
-    var someTabIndex by mutableStateOf<ArticleTabs>(ArticleTabs.TAB_ONE)
-        private set
 
     // Initialize with the current system language
     var currentLanguage: String = Locale.getDefault().language
         private set
 
-    fun setTabIndex(t: ArticleTabs) {
-        someTabIndex = t
-    }
-
     fun setLanguage(languageCode: String) {
         currentLanguage = languageCode
     }
-}
-
-enum class ArticleTabs {
-    TAB_ONE,
-    TAB_TWO,
-    TAB_THREE
 }
